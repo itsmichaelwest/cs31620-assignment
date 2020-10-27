bf6520349ab680d17b28254ee3c6570f6e660ad8
Michael West <19785650+itsmichaelwest@users.noreply.github.com>
2020-10-27 23:07:19 +0000

    Some updates

--- app/src/main/java/uk/ac/aber/dcs/cs31620/phrasepad/MainActivity.kt
+++ app/src/main/java/uk/ac/aber/dcs/cs31620/phrasepad/MainActivity.kt
@@ -1,11 +1,32 @@
 package uk.ac.aber.dcs.cs31620.phrasepad
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.widget.Toolbar
+import uk.ac.aber.dcs.cs31620.phrasepad.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val toolbar = binding.toolbar
+
+        val mOnNavigationItemSelectedListener = Toolbar.OnMenuItemClickListener {item ->
+            when (item.itemId) {
+                R.id.tb_settings -> {
+                    val intent = Intent(this, SettingsActivity::class.java)
+                    startActivity(intent)
+                }
+            }
+            false
+        }
+
+        setSupportActionBar(toolbar)
     }
 }

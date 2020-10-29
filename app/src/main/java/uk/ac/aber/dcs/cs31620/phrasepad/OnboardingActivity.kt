7092f7d4cf7ff91a0b55675f5a712ac9ec5e0a87
Michael West <19785650+itsmichaelwest@users.noreply.github.com>
2020-10-29 12:21:07 +0000

    Updates

--- app/src/main/java/uk/ac/aber/dcs/cs31620/phrasepad/OnboardingActivity.kt
+++ app/src/main/java/uk/ac/aber/dcs/cs31620/phrasepad/OnboardingActivity.kt
@@ -1,11 +1,20 @@
 package uk.ac.aber.dcs.cs31620.phrasepad
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.viewpager2.widget.ViewPager2
+import uk.ac.aber.dcs.cs31620.phrasepad.databinding.ActivityOnboardingBinding
 
 class OnboardingActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityOnboardingBinding
+    private lateinit var pager: ViewPager2
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_onboarding)
+        binding = ActivityOnboardingBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        pager = binding.onboardingPager
     }
 }
